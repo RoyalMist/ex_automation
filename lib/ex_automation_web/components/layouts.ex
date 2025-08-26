e1c25efdf54976f119d01ae8cb6e87669a1e0e3e
Thibault Fouache <tfouache@ripple.com>
2025-08-26 12:27:39 +0200

    Update theme switcher icons and remove unused class on home page

--- lib/ex_automation_web/components/layouts.ex
+++ lib/ex_automation_web/components/layouts.ex
@@ -103,7 +103,7 @@ defmodule ExAutomationWeb.Layouts do
         phx-click={JS.dispatch("phx:set-theme")}
         data-phx-theme="system"
       >
-        <.icon name="hero-computer-desktop-micro" class="size-4 opacity-75 hover:opacity-100" />
+        <.icon name="hero-sparkles-micro" class="size-4 opacity-75 hover:opacity-100" />
       </button>
 
       <button
@@ -111,7 +111,7 @@ defmodule ExAutomationWeb.Layouts do
         phx-click={JS.dispatch("phx:set-theme")}
         data-phx-theme="light"
       >
-        <.icon name="hero-light-bulb-micro" class="size-4 opacity-75 hover:opacity-100" />
+        <.icon name="hero-sun-micro" class="size-4 opacity-75 hover:opacity-100" />
       </button>
 
       <button
@@ -119,7 +119,7 @@ defmodule ExAutomationWeb.Layouts do
         phx-click={JS.dispatch("phx:set-theme")}
         data-phx-theme="dark"
       >
-        <.icon name="hero-moon-micro" class="size-4 opacity-75 hover:opacity-100" />
+        <.icon name="hero-star-micro" class="size-4 opacity-75 hover:opacity-100" />
       </button>
     </div>
     """
